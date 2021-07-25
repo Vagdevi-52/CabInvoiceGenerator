f04fad898372aef59c91e6b981ce88ce9aeb321d
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-25 10:03:19 -0700

    Initial Commit: Welcome To Cab Invoice Generator

---
+++ src/main/java/CabInvoiceGenerator/InvoiceGenerator.java
@@ -0,0 +1,12 @@
+package CabInvoiceGenerator;
+
+public class InvoiceGenerator 
+{
+
+	public static void main(String[] args)
+	{
+       System.out.println("Welcome To Cab Invoice Generator");
+
+	}
+
+}
